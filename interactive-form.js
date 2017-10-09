c9899b6a7b8d24a2a21a0b44e32566f607932225
ollivi <ollivierre.dev@gmail.com>
2017-10-09 17:55:59 -0500

    Commit @ 5:55PM Mon, Oct 9

--- interactive-form.js
+++ interactive-form.js
@@ -1,5 +1,9 @@
 const jobRole = document.querySelector('#title');
 const otherField = document.querySelector('#other-title');
+const design = document.querySelector('#design');
+const color = document.querySelector('#color');
+console.log(design);
+console.log(color[1]);
 otherField.style.display = "none";
 
 //Show and hide input field for the option "other"
@@ -9,4 +13,15 @@ jobRole.addEventListener('change', function() {
   } else {
     otherField.style.display = "none";
   }
-})
+});
+
+//Show and hide input field for the option "other"
+design.addEventListener('change', function() {
+  if (design.value === "js puns") {
+    for (let i = 0; i < color.length; i++) {
+      console.log('Is looping');
+    }
+  } else {
+    //Code block here
+  }
+});
