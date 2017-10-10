8c040c4f6be88e841a036a79af1fab04b0466fb5
ollivi <ollivierre.dev@gmail.com>
2017-10-10 12:49:09 -0500

    Commit @ 12:48PM Tue, Oct 10

--- interactive-form.js
+++ interactive-form.js
@@ -1,9 +1,5 @@
 const jobRole = document.querySelector('#title');
 const otherField = document.querySelector('#other-title');
-const design = document.querySelector('#design');
-const color = document.querySelector('#color');
-console.log(design);
-console.log(color[1]);
 otherField.style.display = "none";
 
 //Show and hide input field for the option "other"
@@ -15,13 +11,38 @@ jobRole.addEventListener('change', function() {
   }
 });
 
+(function() {
+const design = document.querySelector('#design');
+const color = document.querySelector('#color');
+const defaultOption = '<option><-- Select a T-shirt theme</option>';
+color.innerHTML = defaultOption;
+
+const jsPuns =
+  '<option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>' +
+  '<option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option>' +
+  '<option value="gold">Gold (JS Puns shirt only)</option>';
+
+const heartJs =
+  '<option value="tomato">Tomato (I &#9829; JS shirt only)</option>' +
+  '<option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option>' +
+  '<option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option>';
+
+console.log(design);
+console.log(color[1]);
 //Show and hide input field for the option "other"
-design.addEventListener('change', function() {
-  if (design.value === "js puns") {
-    for (let i = 0; i < color.length; i++) {
-      console.log('Is looping');
+  design.addEventListener('change', function() {
+    let options = '<option value="default" selected>Select a Color</option>';
+
+    if (this.value === 'default') {
+      color.innerHTML = defaultOption;
+      return;
     }
-  } else {
-    //Code block here
-  }
-});
+      if (this.value === "js puns") {
+        options += jsPuns;
+        color.innerHTML = options;
+      } else if (this.value === "heart js") {
+        options += heartJs;
+        color.innerHTML = options;
+      }
+  }); //design option EventListener
+}()); //Immediately invoked function
